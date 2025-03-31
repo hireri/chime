bcfa4a3eadceacf52bf9b37c374e952586b6e9ec
avelinia <vel@stoicism.lol>
2025-03-31 15:17:16 +0200

    silly

--- core/exts/context.py
+++ core/exts/context.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 
 class Context(commands.Context):
     async def reply(self, content=None, mention_author=False, **kwargs):
-        return await super().reply(content, **kwargs)
+        return await super().reply(content, mention_author=mention_author, **kwargs)
 
 
 async def get_context_without_mention(self, message, *, cls=Context):
     return await super(type(self), self).get_context(message, cls=cls)
 
 
-def setup(bot):
+async def setup(bot):
     bot.get_context = get_context_without_mention.__get__(bot, type(bot))
+    return
